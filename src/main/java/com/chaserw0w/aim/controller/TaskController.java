8f493d8a303ee15e514da3c3f99cb147a3bda659
Taras Kurenda <chaserw0w.ti@gmail.com>
2023-10-24 21:27:41 +0300

    added insignificant changes

--- src/main/java/com/chaserw0w/aim/controller/TaskController.java
+++ src/main/java/com/chaserw0w/aim/controller/TaskController.java
@@ -4,10 +4,7 @@ import com.chaserw0w.aim.domain.Task;
 import com.chaserw0w.aim.service.TaskService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
